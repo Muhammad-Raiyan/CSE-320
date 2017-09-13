6f3daf9ec6766a0482315197fc7580000747e237
MUHAMMAD ISLAM <mrislam@cs.stonybrook.edu>
2017-09-13 08:44:33 -0400

    Created basic design for Fractionated Morse Cipher

---
+++ hw1/include/fractionated.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <const.h>
+#include "util.h"
+
+void makeFractionatedKey(char* key);
+bool encryptF(char ch);
+bool decryptF(char ch);
+char* textToMorseCode(char ch);
+char* cipherMorseCode(char * ch);
---
+++ hw1/src/fractionated.c
@@ -0,0 +1,21 @@
+#include "fractionated.h"
+
+void makeFractionatedKey(char* key){
+    printf("Key Fractionated\n");
+}
+
+bool encryptF(char ch){
+    return true;
+}
+
+bool decryptF(char ch){
+    return true;
+}
+
+char* textToMorseCode(char ch){
+    return NULL;
+}
+
+char* cipherMorseCode(char * ch){
+    return NULL;
+}
